5f2e48e55a20684f53e6eecd6f98800a8d392edb
rani1029 <ranig1029@gmail.com>
2021-10-03 21:53:36 +0530

    mood Analyse done with test case

---
+++ MoodAnaLyzer/MoodAnalyser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnaLyzer
+{
+    public class MoodAnalyser
+    {
+        string message;
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+        public string AnalyseMood()
+        {
+            if (message.Contains("SAD"))
+            {
+                return "SAD";
+            }
+            else
+                return "HAPPY";
+        }
+    }
+}
---
+++ MoodAnalyserUnitTestt1/UnitTest1.cs
@@ -0,0 +1,34 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoodAnaLyzer;
+
+namespace MoodAnalyserUnitTestt1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        //tc 1.1
+        [TestMethod]
+        public void GivenSadMoodShouldReturnSad()
+        {
+            string message = " i m in a SAD mood";
+            string Expected = "SAD";
+            MoodAnalyser MoodAnalyse = new MoodAnalyser(message);
+            string Mood = MoodAnalyse.AnalyseMood();
+
+            Assert.AreEqual(Expected, Mood);
+
+        }
+        //tc 1.2
+        [TestMethod]
+        public void GivenAnyMoodShouldReturnHappy()
+        {
+            string message = " i m in ANY mood";
+            string Expected = "HAPPY";
+            MoodAnalyser MoodAnalyse = new MoodAnalyser(message);
+            string Mood = MoodAnalyse.AnalyseMood();
+
+            Assert.AreEqual(Expected, Mood);
+
+        }
+    }
+}
