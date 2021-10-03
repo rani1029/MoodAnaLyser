eebccbc46d74d4b965e9bea5cff9405cf3b869c6
rani1029 <ranig1029@gmail.com>
2021-10-04 00:45:07 +0530

    Exception handling done for Null input

---
+++ MoodAnaLyzer/CustomMoodAnalyserException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnaLyzer
+{
+    public class CustomMoodAnalyserException : Exception
+    {
+        public ExceptionType type;
+        public enum ExceptionType
+        {
+            NULL_TYPE_EXCEPTION,
+            EMPTY_TYPE_EXCEPTION
+        }
+        CustomMoodAnalyserException(ExceptionType type, string message) : base(message)
+        {
+            this.type = type;
+        }
+    }
+}
